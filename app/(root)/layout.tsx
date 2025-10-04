29026c1a3d22b9331d7eb3b03f6617ddfcb83c02
AnslemCode <102703239+AnslemCode@users.noreply.github.com>
2025-10-04 03:45:12 +0100

    implement header

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+      <Header />
+      <div className="container py-10">{children}</div>
+    </main>
+  );
+};
+
+export default Layout;
